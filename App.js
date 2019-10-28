86bc3f87cdd9fb4ebee6983810eec136f780e5aa
adelaney11 <adelaney3@luc.edu>
2019-10-28 16:04:57 -0500

    setting up navigation

--- App.js
+++ App.js
@@ -1,133 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-/*
-import React from 'react';
-import {
-  StyleSheet,
-  Button,
-  View,
-  SafeAreaView,
-  Text,
-  Alert,
-} from 'react-native';
-//import Constants from 'expo-constants';
+import {createAppContainer } from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import GameScreen from './src/screens/GameScreen';
 
-function Separator() {
-  return <View style={styles.separator} />;
-}
-
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.title}>Disco Inferno</Text>
-        <Button
-          title="Play Game"
-          onPress={() => Alert.alert('Simple Button pressed')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>1</Text>
-        <Button
-          title="How To Play"
-          color="#f194ff"
-          onPress={() => Alert.alert('Button with adjusted color pressed')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>2</Text>
-        <Button
-          title="Options"
-          onPress={() => Alert.alert('Cannot press this one')}
-        />
-      </View>
-      <Separator />
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    fontSize: 20,
-    //marginTop: Constants.statusBarHeight,
-    marginHorizontal: 16,
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
   },
-  title: {
-    textAlign: 'center',
-    marginVertical: 8,
-    fontSize: 30,
+  Game: {
+    screen: GameScreen,
   },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  Help: {
+    screen: HelpScreen,
   },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
+}, { 
+  initialRouteName: 'Home',
 });
-*/
 
-import React, {Component} from 'react';
-import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {count: 0};
-  }
-
-  onPress = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Disco Inferno</Text>
-        <TouchableOpacity style={styles.button} onPress={this.onPress}>
-          <Text> Play Game </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={this.onPress}>
-          <Text> How To Play </Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={this.onPress}>
-          <Text> Options </Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 10,
-    backgroundColor: '#000000',
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#fcff5e',
-    padding: 20,
-    marginTop: 100,
-  },
-  title: {
-    textAlign: 'center',
-    marginVertical: 8,
-    fontSize: 33,
-    color: '#ffffff',
-    fontFamily: 'Chalkduster',
-  },
-});
+export default createAppContainer(AppNavigator);
