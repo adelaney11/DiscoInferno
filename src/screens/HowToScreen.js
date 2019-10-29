c4a24a77530737ad66ad34c7f9164246725f03f3
ramshaessa <ressa@luc.edu>
2019-10-28 21:02:28 -0500

    Update HowToScreen.js

--- src/screens/HowToScreen.js
+++ src/screens/HowToScreen.js
@@ -1,73 +1,24 @@
-import React, { Component } from 'react';
-import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
 
-const HowToScreen = props => {
-  console.log(props);
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>How To Play</Text>
-      <Text style={styles.text}>
-        <Text style={styles.capitalLetter}>A</Text>
+const HowToScreen = () => {
+    return(
+        <View>
 
-        <Text>
-          re you ready to dance? Disco Inferno is a game that increases in
-          difficulty the farther you progress. Make sure you're ready, because
-          we're gonna go fast!{'\n'}
-          {'\n'}
-          <Text style={styles.capitalLetter}>F</Text>ollow the commands the best
-          you can. If you're slow we will let you know! Your score will decrease
-          until it gets so low you lose!{'\n'}
-          {'\n'}
-          <Text style={styles.capitalLetter}>A</Text>s you kick butt and take
-          names you will advance to new levels. But beware! Levels get
-          progressively harder the further you progress! You got this!
-        </Text>
-      </Text>
-    </View>
-  );
-};
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 10,
-    backgroundColor: '#000000',
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#fcff5e',
-    padding: 20,
-    marginTop: 100,
-  },
-  title: {
-    textAlign: 'center',
-    marginTop: 50,
-    fontSize: 33,
-    color: '#ffffff',
-    fontFamily: 'Chalkduster',
-  },
-  text: {
-    color: '#41cdf4',
-    marginTop: 200,
-    fontSize: 20,
-  },
-  capitalLetter: {
-    color: 'red',
-    fontSize: 20,
-  },
-  wordBold: {
-    fontWeight: 'bold',
-    color: 'black'
-  },
-  italicText: {
-    color: '#37859b',
-    fontStyle: 'italic',
-  },
-  textShadow: {
-    textShadowColor: 'red',
-    textShadowOffset: {width: 2, height: 2},
-    textShadowRadius: 5,
-  },
-});
+            <Image source={require('./')} />;
 
-export default HowToScreen;
+            
+            <Text>How To Play Screen</Text>
+            <Text> 'Do the commands on the screen as fast as you can' </Text>
+            <Text> 'Do not let the tempo of the music distract you!'/> </Text>
+            <Text> 'Be more alert as the frequency of the commands get faster each level'/> </Text>
+            <Text> 'Keep your eyes on the scoreboard and be aware of the lowest score to loose.'/> </Text>
+            <Text> 'After you reach the score required to move on to the next level, you conquer the previous level'/> </Text>
+            <Text> 'Happy gaming and follow the instructions above to proceed successfully in the game'/> </Text>
+    
+        </View>
+
+    )
+}
+
+export default HowToScreen; 
