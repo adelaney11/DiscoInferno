de45019c0fd928a570ec2b2ba1bdfd87ba75d534
adelaney11 <adelaney3@luc.edu>
2019-10-31 13:53:58 -0500

    navigation fix

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Disco Inferno</Text>
