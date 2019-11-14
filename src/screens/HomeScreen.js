4a2ff1889c25899d6eb5964d72cc1c3a9c6597a2
descartes90 <jmurphy29@luc.edu>
2019-11-14 16:27:15 -0600

    Styled home screen better

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,52 +1,104 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
 
-const HomeScreen = (props) => {
-    return (
+const HomeScreen = props => {
+  return (
+    <ImageBackground
+      source={require('/Users/joshua/DiscoInferno/assets/coolFire4.jpg')}
+      style={styles.background}>
       <View style={styles.container}>
         <Text style={styles.title}>Disco Inferno</Text>
-        
-        <TouchableOpacity 
-        style={styles.button}
-        onPress = {() => props.navigation.navigate('Game')}>
-            <Text>Play</Text>
-        </TouchableOpacity> 
 
-        <TouchableOpacity 
-        style={styles.button}
-        onPress = {() => props.navigation.navigate('HowTo')}>
-          <Text> How To Play </Text>
+        <TouchableOpacity
+          //style={styles.play}
+          onPress={() => props.navigation.navigate('Game')}>
+          <Text style={styles.play}>Play</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity 
-        style={styles.button}
-        onPress = {() => props.navigation.navigate('Options')}>
-          <Text> Options </Text>
+        <TouchableOpacity
+          //style={styles.howToPlay}
+          onPress={() => props.navigation.navigate('HowTo')}>
+          <Text style={styles.howToPlay}>How To Play </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          //style={styles.options}
+          onPress={() => props.navigation.navigate('Options')}>
+          <Text style={styles.options}>Options</Text>
         </TouchableOpacity>
       </View>
-    )
+    </ImageBackground>
+  );
 };
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      paddingHorizontal: 10,
-      backgroundColor: '#000000',
-    },
-    button: {
-      alignItems: 'center',
-      backgroundColor: '#fcff5e',
-      padding: 20,
-      marginTop: 100,
-    },
-    title: {
-      textAlign: 'center',
-      marginVertical: 8,
-      fontSize: 33,
-      color: '#ffffff',
-      fontFamily: 'Futura',
-    },
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  play: {
+    backgroundColor: 'white',
+    color: '#3A59FF',
+    width: '75%',
+    borderRadius: 27,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginLeft: '11%',
+    padding: '2%',
+    fontSize: 27,
+    marginTop: '40%',
+    //borderWidth: 1,
+  },
+  howToPlay: {
+    backgroundColor: '#3A59FF',
+    color: 'white',
+    width: '75%',
+    borderRadius: 27,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginLeft: '11%',
+    padding: '2%',
+    fontSize: 27,
+    marginTop: '10%',
+    //borderWidth: 1,
+  },
+  options: {
+    backgroundColor: 'white',
+    color: '#3A59FF',
+    width: '75%',
+    borderRadius: 27,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginLeft: '11%',
+    padding: '2%',
+    fontSize: 27,
+    marginTop: '10%',
+    //borderWidth: 1,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+    fontSize: 50,
+    color: '#ffffff',
+    fontFamily: 'Copperplate-bold',
+  },
+  background: {
+    width: '100%',
+    height: '100%',
+  },
+  logo: {
+    width: 280,
+    height: 280,
+    marginLeft: '15%',
+    marginTop: '10%',
+  },
+});
 
 export default HomeScreen;
